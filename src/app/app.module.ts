cfa4b527b3cf5ccbcc41ec1c436165d472e94788
Ami Friedman <amishosh@gmail.com>
2019-03-01 14:07:30 +0200

    Mosh: Component generation and some navbar refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -13,6 +14,12 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { UsernameComponent } from './username/username.component';
 import { environment } from 'src/environments/environment';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +27,14 @@ import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
     HomeComponent,
     ShoppingCartComponent,
     UsernameComponent,
-    BsNavbarComponent
+    BsNavbarComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +43,13 @@ import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
     AngularFireAuthModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'shopping-cart', component: ShoppingCartComponent }
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent },
+      { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin/products', component: AdminProductsComponent },
+      { path: 'admin/orders', component: AdminOrdersComponent },
     ]),
     AppRoutingModule,
     NgbModule,
