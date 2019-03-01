4bd223ea39d491845bea6b4d18e0ea4c9a9c8da8
Ami Friedman <amishosh@gmail.com>
2019-03-01 13:15:08 +0200

    Before Mosh's solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShoppingCartComponent,
+    UsernameComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent }
+    ]),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
